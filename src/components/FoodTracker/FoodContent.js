1a524b2fdda30e71a4b4d8252ea10b25aa1f8f4b
Fahad Hussain <sfahadh14@gmail.com>
2019-04-20 20:02:45 -0400

    centered food search and content components to center

--- src/components/FoodTracker/FoodContent.js
+++ src/components/FoodTracker/FoodContent.js
@@ -7,19 +7,19 @@ export default class FoodContent extends Component {
       <div>
         <FoodSearch />
         <div className="nutrition-content">
-            <div className="box">Nutrient</div>
+            <div className="box left">Nutrient</div>
             <div className="box">Unit</div>
             <div className="box">Weight</div>
-            <div className="box">Energy</div>
+            <div className="box left">Energy</div>
             <div className="box">-</div>
             <div className="box">-</div>
-            <div className="box">Protein</div>
+            <div className="box left">Protein</div>
             <div className="box">-</div>
             <div className="box">-</div>
-            <div className="box">Carbohydrate</div>
+            <div className="box left">Carbohydrate</div>
             <div className="box">-</div>
             <div className="box">-</div>
-            <div className="box bottom">Fat</div>
+            <div className="box bottom left">Fat</div>
             <div className="box bottom">-</div>
             <div className="box bottom">-</div>
         </div>
