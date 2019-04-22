28c83ef0322f25e466598e0bb6263618917950ca
Fahad Hussain <sfahadh14@gmail.com>
2019-04-22 13:45:18 -0400

    fixed error in which filtered food repeats

--- src/components/FoodTracker/FoodContent.js
+++ src/components/FoodTracker/FoodContent.js
@@ -11,25 +11,10 @@ export default class FoodContent extends Component {
     this.handleSearchInput = this.handleSearchInput.bind(this)
   }
 
-  // findFood() {
-  //   let foodList = this.props.nutrientContent.map(names => names.name);
-  //   // console.log(foodNames)
-  //   // for(let food in foodList) {
-  //     if(this.state.foodName === foodList) {
-  //       console.log(this.state.foodName)
-  //     } else {
-  //       console.log("ERROR! Wrong food")
-  //     }
-  //   // }
-  //   // console.log(foodNames)
-  //   // let nutrientInfo = nutrientContent.map(nutrients => nutrients.nutrients)
-  //   // console.log(nutrientInfo[0]);
-  // }
-
   handleSearchInput(e) {
     console.log(e.target.value)
-    let filteredFood = this.props.nutrientContent.map(food => food.name).filter(searchedFood => {
-      return searchedFood.toLowerCase().search(e.target.value.toLowerCase()) !== -1;
+    let filteredFood = this.props.nutrientContent.filter(searchedFood => {
+      return searchedFood.name.toLowerCase().search(e.target.value.toLowerCase()) !== -1;
     })
     this.setState({
       foodName: filteredFood
@@ -37,9 +22,6 @@ export default class FoodContent extends Component {
   }
 
   render() {
-    // console.log(this.props.nutrientContent.map(food => food.name))
-    // .indexOf(this.state.foodName !== -1);
-    // this.findFood();
     const {nutrientContent, fetchFoodData} = this.props
     return (
       <div>
@@ -47,10 +29,6 @@ export default class FoodContent extends Component {
           nutrientContent={nutrientContent} 
           searchfood={this.handleSearchInput}
         />
-        {
-          nutrientContent.map(foodNames => {
-          return <li id="food-name" key={foodNames.ndbno}>{this.state.foodName}</li>})
-        }
         <div className="nutrition-content">
             <div className="box left">Nutrient</div>
             <div className="box">Unit</div>
@@ -77,6 +55,10 @@ export default class FoodContent extends Component {
             <div className="box bottom">-</div>
             <div className="box bottom">-</div>
         </div>
+        {
+          this.state.foodName.map(foodNames => {
+          return <li id="food-name" key={foodNames.ndbno}>{foodNames.name}</li>})
+        }
       </div>
     )
   }
