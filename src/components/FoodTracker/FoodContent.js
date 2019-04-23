4226550aafc81417de2093cd5fc54a5e3e0418bb
Fahad Hussain <sfahadh14@gmail.com>
2019-04-23 14:47:15 -0400

    displays food data on table

--- src/components/FoodTracker/FoodContent.js
+++ src/components/FoodTracker/FoodContent.js
@@ -8,7 +8,14 @@ export default class FoodContent extends Component {
 
     this.state = {
       foodNames: [],
-      selectFood: []
+      selectFood: {
+        nutrients: [
+          {calories: 0},
+          {protein: 0},
+          {carbs: 0},
+          {fats: 0},
+        ]
+      }
     }
     this.handleSearchInput = this.handleSearchInput.bind(this)
   }
@@ -23,13 +30,9 @@ export default class FoodContent extends Component {
     })
   }
 
-  // displayFood(table) {
-  //   if(this.state.foodName.length === 1) {
-  //     return table
-  //   } else {
-  //     console.log("too many foods")
-  //   }
-  // }
+  displayTable(table) {
+    return table;
+  }
 
   displayFoodData = (e, table) => {
     for(let i = 0; i < this.state.foodNames.length; i++) {
@@ -37,15 +40,18 @@ export default class FoodContent extends Component {
         this.setState({
           selectFood: this.state.foodNames[i]
         })
-        return table;
+        this.displayTable(table);
+        break;
       }
     }
   }
 
   render() {
     const {nutrientContent} = this.props
-    const {foodNames} = this.state
-    console.log(this.state.selectFood)
+    const {foodNames, selectFood} = this.state
+    console.log(selectFood)
+    console.log(selectFood[0])
+    console.log(selectFood.weight)
 
     let table = (
       <div className="nutrition-content">
@@ -56,31 +62,31 @@ export default class FoodContent extends Component {
 
         <div className="box left">Energy</div>
         <div className="box">kcal</div>
-        <div className="box">{(() => {if(foodNames[0] !== undefined) 
-        return <p>{foodNames[0].weight}</p>})()}</div>
-        <div className="box">{(() => {if(foodNames[0] !== undefined) 
-        return <p>{foodNames[0].nutrients[0].value}</p>})()}</div>
+        <div className="box">{(() => {if(selectFood !== undefined) 
+        return <p>{selectFood.weight}</p>})()}</div>
+        <div className="box">{(() => {if(selectFood !== undefined) 
+        return <p>{selectFood.nutrients[0].value}</p>})()}</div>
 
         <div className="box left">Protein</div>
         <div className="box">g</div>
-        <div className="box">{(() => {if(foodNames[0] !== undefined) 
-        return <p>{parseInt(Math.round((foodNames[0].nutrients[1].value)) * 4) / 4}</p>})()}</div>
-        <div className="box">{(() => {if(foodNames[0] !== undefined) 
-        return <p>{parseInt(Math.round(foodNames[0].nutrients[1].value)) * 4}</p>})()}</div>
+        <div className="box">{(() => {if(selectFood !== undefined) 
+        return <p>{parseInt(Math.round((selectFood.nutrients[1].value)) * 4) / 4}</p>})()}</div>
+        <div className="box">{(() => {if(selectFood !== undefined) 
+        return <p>{parseInt(Math.round(selectFood.nutrients[1].value)) * 4}</p>})()}</div>
 
         <div className="box left">Carbohydrate</div>
         <div className="box">g</div>
-        <div className="box">{(() => {if(foodNames[0] !== undefined) 
-        return <p>{parseInt(Math.round((foodNames[0].nutrients[3].value)) * 4) / 4}</p>})()}</div>
-        <div className="box">{(() => {if(foodNames[0] !== undefined) 
-        return <p>{parseInt(Math.round(foodNames[0].nutrients[3].value)) * 4}</p>})()}</div>
+        <div className="box">{(() => {if(selectFood !== undefined) 
+        return <p>{parseInt(Math.round((selectFood.nutrients[3].value)) * 4) / 4}</p>})()}</div>
+        <div className="box">{(() => {if(selectFood !== undefined) 
+        return <p>{parseInt(Math.round(selectFood.nutrients[3].value)) * 4}</p>})()}</div>
 
         <div className="box left bottom">Fat</div>
         <div className="box bottom">g</div>
-        <div className="box bottom">{(() => {if(foodNames[0] !== undefined) 
-        return <p>{parseInt(Math.round((foodNames[0].nutrients[2].value)) * 9) / 9}</p>})()}</div>
-        <div className="box bottom">{(() => {if(foodNames[0] !== undefined) 
-        return <p>{parseInt(Math.round(foodNames[0].nutrients[2].value)) * 9}</p>})()}</div>
+        <div className="box bottom">{(() => {if(selectFood !== undefined) 
+        return <p>{parseInt(Math.round((selectFood.nutrients[2].value)) * 9) / 9}</p>})()}</div>
+        <div className="box bottom">{(() => {if(selectFood !== undefined) 
+        return <p>{parseInt(Math.round(selectFood.nutrients[2].value)) * 9}</p>})()}</div>
       </div>
     )
 
@@ -89,8 +95,10 @@ export default class FoodContent extends Component {
         <FoodSearch 
           nutrientContent={nutrientContent} 
           searchfood={this.handleSearchInput}
+          foodNames={foodNames}
+          selectFood={selectFood}
         />
-          {/* {this.displayFood(table)} */}
+        {this.displayTable(table)}
         {
           foodNames.map(foodName => {
           return <li id="food-name" key={foodName.ndbno}>
