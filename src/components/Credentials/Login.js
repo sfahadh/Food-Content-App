4a810d2235df10927beaa062f08a45afd98afaad
Fahad Hussain <sfahadh14@gmail.com>
2019-08-05 18:11:36 -0400

    removed credential s css file

--- src/components/Credentials/Login.js
+++ src/components/Credentials/Login.js
@@ -1,85 +1,82 @@
-import React, { Component } from 'react'
-import './Credentials.css';
-import { Redirect } from "react-router-dom"
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 
 export default class Login extends Component {
-    constructor() {
-        super()
+  constructor() {
+    super();
 
-        this.state = {
-            username: "",
-            password: "",
-            myUsername: "fahad",
-            myPassword: "idk",
-            isUsername: false,
-            isPassword: false,
-            open: true,
-            isLoggedIn: false
-        }
-        this.validatePassword = this.validatePassword.bind(this);
-        this.validateUsername = this.validateUsername.bind(this);
-        this.validateCredentials = this.validateCredentials.bind(this);
+    this.state = {
+      username: "",
+      password: "",
+      myUsername: "fahad",
+      myPassword: "idk",
+      isUsername: false,
+      isPassword: false,
+      open: true,
+      isLoggedIn: false
+    };
+    this.validatePassword = this.validatePassword.bind(this);
+    this.validateUsername = this.validateUsername.bind(this);
+    this.validateCredentials = this.validateCredentials.bind(this);
+  }
+
+  show = dimmer => () => this.setState({ dimmer, open: true });
+
+  close = () => this.setState({ open: false });
+
+  validateUsername(e) {
+    if (e.target.value === this.state.myUsername)
+      this.setState({
+        isUsername: true
+      });
+    this.validatePassword(e);
+  }
+
+  validatePassword(e) {
+    if (e.target.value === this.state.myPassword)
+      this.setState({
+        isPassword: true
+      });
+  }
+
+  validateCredentials(e) {
+    e.preventDefault();
+    if (!!this.state.isUsername && !!this.state.isPassword) {
+      this.setState({
+        isLoggedIn: true
+      });
+    } else {
+      alert("WRONG");
     }
+  }
 
-    show = dimmer => () => this.setState({ dimmer, open: true })
-    
-    close = () => this.setState({ open: false })
-
-    validateUsername(e) {
-        if(e.target.value === this.state.myUsername) 
-            this.setState({
-                isUsername: true
-            })
-            this.validatePassword(e);
-    }
-
-    validatePassword(e) {
-        if(e.target.value === this.state.myPassword) 
-            this.setState({
-                isPassword: true
-            })
-    }
-
-    validateCredentials(e) {
-        e.preventDefault();
-        if(!!this.state.isUsername && !!this.state.isPassword) {
-            this.setState({
-                isLoggedIn: true
-            })
-        } else {
-            alert("WRONG");
-        }
-    }
-
-    loginButton(e) {
-        e.preventDefault();
-    }
+  loginButton(e) {
+    e.preventDefault();
+  }
 
   render() {
-    if(this.state.isLoggedIn) {
-        return <Redirect to="/profile/personal"/>
+    if (this.state.isLoggedIn) {
+      return <Redirect to="/profile/personal" />;
     }
 
     return (
       <div id="login-background">
         <div id="loginForm">
-            <img id="logo-image" src={require("../../images/logo.png")} alt="profile"></img>
-            <h2>Nutri Gainz</h2>
-            <form className="login">
-                <div className="loginFields">
-                    <input 
-                        placeholder="username"
-                        onChange={this.validateUsername}
-                    ></input>
-                    <input 
-                        placeholder="password"
-                        onChange={this.validatePassword}
-                    ></input>
-                    <button onClick={this.validateCredentials}>LOGIN</button>
-                </div>
-            </form>
+          <img
+            id="logo-image"
+            src={require("../../images/logo.png")}
+            alt="profile"
+          />
+          <h2>Nutri Gainz</h2>
+          <form className="login">
+            <div className="loginFields">
+              <input placeholder="username" onChange={this.validateUsername} />
+              <input placeholder="password" onChange={this.validatePassword} />
+              <button onClick={this.validateCredentials}>LOGIN</button>
+            </div>
+          </form>
         </div>
       </div>
-    )
+    );
   }
 }
