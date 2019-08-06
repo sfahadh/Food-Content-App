16e9ce77dd53a8b236de5fc4fb2c8f5087c51932
Fahad Hussain <sfahadh14@gmail.com>
2019-08-06 11:27:57 -0400

    removed vw and vh with px in login page

--- src/components/Credentials/Register.js
+++ src/components/Credentials/Register.js
@@ -1,25 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Register extends Component {
   render() {
     return (
-        <div>
-            <h2>Login:</h2>
-            <form className="login">
-                <div className="loginFields">
-                    <input 
-                        placeholder="username"
-                        onChange={this.validateUsername}
-                    ></input>
-                    <input 
-                        placeholder="password"
-                        onChange={this.validatePassword}
-                    ></input>
-                    <button onClick={this.validateCredentials}>LOGIN</button>
-                </div>
-            </form>
-            <Personal/>
-        </div>
-    )
+      <div>
+        <h2>Login:</h2>
+        <form className="login">
+          <div className="loginFields">
+            <input placeholder="username" onChange={this.validateUsername} />
+            <input placeholder="password" onChange={this.validatePassword} />
+            <button onClick={this.validateCredentials}>LOGIN</button>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
