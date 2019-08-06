23ea46c7a3f2e702b8d18f9dd0ee678a82a9190d
Fahad Hussain <sfahadh14@gmail.com>
2019-08-06 17:25:49 -0400

    removed all functions, state, and constructor in personal file

--- src/components/Profile/Personal.js
+++ src/components/Profile/Personal.js
@@ -2,37 +2,7 @@ import React, { Component } from "react";
 import "./Profile.css";
 
 export default class Personal extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      firstName: "",
-      lastName: "",
-      emailAddress: "",
-      phoneNumber: "",
-      dateOfBirth: "",
-      streetAddress: ""
-    };
-    this.handleProfileInput = this.handleProfileInput.bind(this);
-  }
-
-  handleProfileInput(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  }
-
-  storePersonalInfo() {
-    localStorage.setItem("firstName", this.state.firstName);
-    localStorage.setItem("lastName", this.state.lastName);
-    localStorage.setItem("emailAddress", this.state.emailAddress);
-    localStorage.setItem("phoneNumber", this.state.phoneNumber);
-    localStorage.setItem("dateOfBirth", this.state.dateOfBirth);
-    localStorage.setItem("streetAddress", this.state.streetAddress);
-  }
-
   render() {
-    this.storePersonalInfo();
     return <div id="profile-background" />;
   }
 }
