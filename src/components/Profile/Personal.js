551016ff67cc4891bf5f2bdcd41079b737e74502
Fahad Hussain <sfahadh14@gmail.com>
2019-08-06 17:31:25 -0400

    added under construction image in personal js file

--- src/components/Profile/Personal.js
+++ src/components/Profile/Personal.js
@@ -1,6 +1,14 @@
 import React from "react";
 import "./Profile.css";
 
-const Personal = () => <div id="profile-background" />;
+const Personal = () => (
+  <div id="profile-background">
+    <img
+      id="construction"
+      src={require("../../images/under-construction.png")}
+      alt="Under Construction"
+    />
+  </div>
+);
 
 export default Personal;
